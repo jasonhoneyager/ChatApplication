1b7b661932f54d7d156070a0525cba519aabe5f5
Jason Honeyager <jasonhoneyager@gmail.com>
2016-08-13 21:09:44 -0500

    Initial commit. reworking of original Chat program ChatServer and ChatClient.  Now running in C# Windows Forms.  Chats are distributed to all participants.

---
+++ ChatClient/ChatClient/Client.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ChatClient
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputName = new System.Windows.Forms.TextBox();
+            this.InputServerIP = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.ChatWindow = new System.Windows.Forms.RichTextBox();
+            this.InputMessage = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputName
+            // 
+            this.InputName.Location = new System.Drawing.Point(75, 27);
+            this.InputName.Name = "InputName";
+            this.InputName.Size = new System.Drawing.Size(150, 20);
+            this.InputName.TabIndex = 1;
+            // 
+            // InputServerIP
+            // 
+            this.InputServerIP.Location = new System.Drawing.Point(91, 53);
+            this.InputServerIP.Name = "InputServerIP";
+            this.InputServerIP.Size = new System.Drawing.Size(134, 20);
+            this.InputServerIP.TabIndex = 3;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(231, 27);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(131, 20);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // ChatWindow
+            // 
+            this.ChatWindow.Location = new System.Drawing.Point(12, 109);
+            this.ChatWindow.Name = "ChatWindow";
+            this.ChatWindow.ReadOnly = true;
+            this.ChatWindow.Size = new System.Drawing.Size(368, 268);
+            this.ChatWindow.TabIndex = 1;
+            this.ChatWindow.Text = "";
+            // 
+            // InputMessage
+            // 
+            this.InputMessage.Location = new System.Drawing.Point(8, 22);
+            this.InputMessage.Multiline = true;
+            this.InputMessage.Name = "InputMessage";
+            this.InputMessage.Size = new System.Drawing.Size(262, 57);
+            this.InputMessage.TabIndex = 0;
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(276, 22);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(86, 57);
+            this.SendButton.TabIndex = 1;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ExitButton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.InputName);
+            this.groupBox1.Controls.Add(this.InputServerIP);
+            this.groupBox1.Controls.Add(this.ConnectButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(368, 91);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chat Options";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Chat Server IP:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.InputMessage);
+            this.groupBox2.Controls.Add(this.SendButton);
+            this.groupBox2.Location = new System.Drawing.Point(12, 383);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(368, 90);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Message";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(231, 53);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(131, 21);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 479);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ChatWindow);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Client";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Super Basic Chat Program";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Client_FormClosing);
+            this.Load += new System.EventHandler(this.Client_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputName;
+        private System.Windows.Forms.TextBox InputServerIP;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.RichTextBox ChatWindow;
+        private System.Windows.Forms.TextBox InputMessage;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ ChatServer/ChatServer/Server.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ChatServer
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServerTraffic = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ServerStatus = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ServerTraffic
+            // 
+            this.ServerTraffic.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ServerTraffic.Location = new System.Drawing.Point(12, 33);
+            this.ServerTraffic.Name = "ServerTraffic";
+            this.ServerTraffic.ReadOnly = true;
+            this.ServerTraffic.Size = new System.Drawing.Size(412, 219);
+            this.ServerTraffic.TabIndex = 0;
+            this.ServerTraffic.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Status:";
+            // 
+            // ServerStatus
+            // 
+            this.ServerStatus.AutoSize = true;
+            this.ServerStatus.Location = new System.Drawing.Point(58, 9);
+            this.ServerStatus.Name = "ServerStatus";
+            this.ServerStatus.Size = new System.Drawing.Size(0, 13);
+            this.ServerStatus.TabIndex = 2;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.Location = new System.Drawing.Point(349, 258);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 288);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ServerStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ServerTraffic);
+            this.Name = "Server";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ServerTraffic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ServerStatus;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
